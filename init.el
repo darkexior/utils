3507f630c9bfa5f611b37de75e79f862144008d4
Gabriel Guitián Estrella <39809625+darkexior@users.noreply.github.com>
2024-12-18 20:45:07 -0400

    Update init.el

--- init.el
+++ init.el
@@ -33,18 +33,3 @@
 (global-display-line-numbers-mode t)
 (custom-set-faces
   '(mode-line ((t (:background "black :foreground "gray70" :box (:line-width 4 :color "black"))))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176" "1c12bf9e876db603c94ecba530ae4ad5c899d46944ef6f3ce7918cfc07c6564a" "ecf898411a1e04d9f431febe2c5b6643eab97128b33823b345e4169dfb3889e0" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-"# utils" 
