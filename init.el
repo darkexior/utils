f1d2b3082e5693fee22e58181a5236d9f9bb3a43
Gabriel Guitián Estrella <39809625+darkexior@users.noreply.github.com>
2024-12-18 20:50:38 -0400

    Update init.el

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;(set-face-attribute 'default nil :font "Borg Sans Mono")
 (setq inhibit-startup-message t)
-(setq default-directory "C:/Users/Guest/Gabriel")
+(setq default-directory "")
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
